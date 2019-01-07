e76408b40831a218f65c7372f381f0c96fba21da
wildbuffalo <feirenliu@gmail.com>
2019-01-07 16:54:32 -0500

    update jenkins

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -5,26 +5,26 @@ folder(basePath) {
     description 'This example shows basic folder/job creation.'
 }
 
-job("$basePath/gradle-example-build") {
-    scm {
-        github repo
-    }
-    triggers {
-        scm 'H/5 * * * *'
-    }
-    steps {
-        gradle 'assemble'
-    }
-}
-
-job("$basePath/gradle-example-deploy") {
-    parameters {
-        stringParam 'host'
-    }
-    steps {
-        shell 'scp war file; restart...'
-    }
-}
+//job("$basePath/gradle-example-build") {
+//    scm {
+//        github repo
+//    }
+//    triggers {
+//        scm 'H/5 * * * *'
+//    }
+//    steps {
+//        gradle 'assemble'
+//    }
+//}
+//
+//job("$basePath/gradle-example-deploy") {
+//    parameters {
+//        stringParam 'host'
+//    }
+//    steps {
+//        shell 'scp war file; restart...'
+//    }
+//}
 
 pipelineJob('foo') {
     definition {
