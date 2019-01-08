caa38ff18cd8863bed117f3af697acb505bd7e63
wildbuffalo <feirenliu@gmail.com>
2019-01-08 10:29:46 -0500

    update jenkins

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -85,3 +85,137 @@ nestedView(basePath) {
 ////        }
 //    }
 //}
+sectionedView('project-summary') {
+    filterBuildQueue()
+    filterExecutors()
+    sections {
+        listView {
+            name('Project A')
+            jobs {
+                regex(/project-A-.*/)
+            }
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+            }
+        }
+        listView {
+            name('Project B')
+            jobs {
+                regex(/project-B-.*/)
+            }
+            jobFilters {
+                regex {
+                    matchValue(RegexMatchValue.DESCRIPTION)
+                    regex(/.*-project-B-.*/)
+                }
+            }
+            columns {
+                status()
+                weather()
+                name()
+                lastSuccess()
+                lastFailure()
+            }
+        }
+    }
+}
+listView('project-A') {
+    description('All unstable jobs for project A')
+    filterBuildQueue()
+    filterExecutors()
+    jobs {
+        name('release-projectA')
+        regex(/project-A-.+/)
+    }
+    jobFilters {
+        status {
+            status(Status.UNSTABLE)
+        }
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
+deliveryPipelineView('project-a') {
+    pipelineInstances(5)
+    showAggregatedPipeline()
+    columns(2)
+    sorting(Sorting.TITLE)
+    updateInterval(60)
+    enableManualTriggers()
+    showAvatars()
+    showChangeLog()
+    pipelines {
+        component('Sub System A', 'compile-a')
+        component('Sub System B', 'compile-b')
+        regex(/compile-subsystem-(.*)/)
+    }
+}
+dashboardView('example') {
+    jobs {
+        regex(/acme-.*/)
+    }
+    columns {
+        status()
+        weather()
+        buildButton()
+    }
+    topPortlets {
+        jenkinsJobsList {
+            displayName('acme jobs')
+        }
+    }
+    leftPortlets {
+        testStatisticsChart()
+    }
+    rightPortlets {
+        testTrendChart()
+    }
+    bottomPortlets {
+        iframe {
+            effectiveUrl('http://example.com')
+        }
+        testStatisticsGrid()
+        buildStatistics()
+    }
+}
+categorizedJobsView('example') {
+    jobs {
+        regex(/configuration_.*/)
+    }
+    categorizationCriteria {
+        regexGroupingRule(/^configuration_([^_]+).*$/)
+    }
+    columns {
+        status()
+        categorizedJob()
+        buildButton()
+    }
+}
+buildPipelineView('project-A') {
+    filterBuildQueue()
+    filterExecutors()
+    title('Project A CI Pipeline')
+    displayedBuilds(5)
+    selectedJob('project-A-compile')
+    alwaysAllowManualTrigger()
+    showPipelineParameters()
+    refreshFrequency(60)
+}
+buildMonitorView('project-A') {
+    description('All jobs for project A')
+    jobs {
+        name('release-projectA')
+        regex(/project-A-.+/)
+    }
+}
