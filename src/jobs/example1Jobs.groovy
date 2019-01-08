619b4d7b6d3d567867ef1107cd2d4b7f27420b5a
wildbuffalo <feirenliu@gmail.com>
2019-01-08 11:25:43 -0500

    update jenkins

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -85,44 +85,44 @@ nestedView(basePath) {
 ////        }
 //    }
 //}
-sectionedView('project-summary') {
-    filterBuildQueue()
-    filterExecutors()
-    sections {
-        listView {
-            name('Project A')
-            jobs {
-                regex(/project-A-.*/)
-            }
-            columns {
-                status()
-                weather()
-                name()
-                lastSuccess()
-                lastFailure()
-            }
-        }
-        listView {
-            name('Project B')
-            jobs {
-                regex(/project-B-.*/)
-            }
-            jobFilters {
-                regex {
-                    matchValue(RegexMatchValue.DESCRIPTION)
-                    regex(/.*-project-B-.*/)
-                }
-            }
-            columns {
-                status()
-                weather()
-                name()
-                lastSuccess()
-                lastFailure()
-            }
-        }
-    }
-}
+//sectionedView('project-summary') {
+//    filterBuildQueue()
+//    filterExecutors()
+//    sections {
+//        listView {
+//            name('Project A')
+//            jobs {
+//                regex(/project-A-.*/)
+//            }
+//            columns {
+//                status()
+//                weather()
+//                name()
+//                lastSuccess()
+//                lastFailure()
+//            }
+//        }
+//        listView {
+//            name('Project B')
+//            jobs {
+//                regex(/project-B-.*/)
+//            }
+//            jobFilters {
+//                regex {
+//                    matchValue(RegexMatchValue.DESCRIPTION)
+//                    regex(/.*-project-B-.*/)
+//                }
+//            }
+//            columns {
+//                status()
+//                weather()
+//                name()
+//                lastSuccess()
+//                lastFailure()
+//            }
+//        }
+//    }
+//}
 listView('project-A') {
     description('All unstable jobs for project A')
     filterBuildQueue()
