4420d7fc24f1c1b2f6183253890f771f69aa4876
wildbuffalo <feirenliu@gmail.com>
2019-01-08 11:29:29 -0500

    update jenkins

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -146,21 +146,7 @@ listView('project-A') {
         buildButton()
     }
 }
-deliveryPipelineView('project-a') {
-    pipelineInstances(5)
-    showAggregatedPipeline()
-    columns(2)
-    sorting(Sorting.TITLE)
-    updateInterval(60)
-    enableManualTriggers()
-    showAvatars()
-    showChangeLog()
-    pipelines {
-        component('Sub System A', 'compile-a')
-        component('Sub System B', 'compile-b')
-        regex(/compile-subsystem-(.*)/)
-    }
-}
+
 dashboardView('example') {
     jobs {
         regex(/acme-.*/)
