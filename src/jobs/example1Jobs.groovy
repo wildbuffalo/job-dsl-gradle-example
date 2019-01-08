9bc32ffa73563ced73478e87dd935a4e6f093268
wildbuffalo <feirenliu@gmail.com>
2019-01-08 11:42:09 -0500

    update jenkins

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -147,34 +147,6 @@ listView('project-A') {
     }
 }
 
-dashboardView('example') {
-    jobs {
-        regex(/acme-.*/)
-    }
-    columns {
-        status()
-        weather()
-        buildButton()
-    }
-    topPortlets {
-        jenkinsJobsList {
-            displayName('acme jobs')
-        }
-    }
-    leftPortlets {
-        testStatisticsChart()
-    }
-    rightPortlets {
-        testTrendChart()
-    }
-    bottomPortlets {
-        iframe {
-            effectiveUrl('http://example.com')
-        }
-        testStatisticsGrid()
-        buildStatistics()
-    }
-}
 categorizedJobsView('example') {
     jobs {
         regex(/configuration_.*/)
