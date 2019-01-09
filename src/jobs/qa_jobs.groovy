32b2e78093354cd7d3502d56dd67c3bacdbcfe83
wildbuffalo <feirenliu@gmail.com>
2019-01-09 13:21:52 -0500

    update jenkins

--- src/jobs/qa_jobs.groovy
+++ src/jobs/qa_jobs.groovy
@@ -5,7 +5,7 @@ folder(basePath) {
     description 'This example shows how to create a set of jobs for each github branch, each in its own folder.'
 }
 
-job('QA-acgg') {
+job("$basePath/QA-acgg") {
     logRotator(-1, 10)
     scm {
         github('wildbuffalo/getting-stated-nodejs','master')
