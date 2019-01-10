4e99c2562619d6d5ce531c9da9db50761d835bf6
wildbuffalo <feirenliu@gmail.com>
2019-01-10 11:46:49 -0500

    update jenkins

--- src/jobs/example1Jobs.groovy
+++ src/jobs/example1Jobs.groovy
@@ -57,34 +57,34 @@ pipelineJob("$basePath/QA-dealworks-app") {
 }
 
 pipelineJob("$basePath/deployment") {
-definition {
+    definition {
 //        cps {
 //            script(readFileFromWorkspace('src/jobs/src/project-a-workflow.groovy'))
 //            sandbox()
 //        }
-    cpsScm {
-        scm {
-            git {
-                remote {
-                    branch('master')
-                    github('wildbuffalo/job-dsl-gradle-example')
-                    credentials('github-user')
-                }
-                extensions {
-                    cleanAfterCheckout()
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        branch('master')
+                        github('wildbuffalo/job-dsl-gradle-example')
+                        credentials('github-user')
+                    }
+                    extensions {
+                        cleanAfterCheckout()
 //                        relativeTargetDirectory('repo1')
-                }
+                    }
 //                    scriptPath('src/jobs/src/project-a-workflow.groovy')
-            }
+                }
 //                github('wildbuffalo/getting-stated-nodejs')
+            }
+            scriptPath('src/jobs/src/deploy.groovy')
         }
-        scriptPath('src/jobs/src/deploy.groovy')
+    }
+    triggers {
+        githubPush()
     }
 }
-triggers {
-    githubPush()
-}
-
 //pipelineJob('build-dealworks-app') {
 //    definition {
 ////        cps {
