319d5c1a08f9b78acc497474852ee20f69ccd04d
wildbuffalo <feirenliu@gmail.com>
2019-01-11 11:57:39 -0500

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- src/jobs/build_jobs.groovy
+++ src/jobs/build_jobs.groovy
@@ -5,64 +5,65 @@ folder(basePath) {
     description 'This example shows how to create a set of jobs for each github branch, each in its own folder.'
 }
 
-//pipelineJob('build-dealworks-app') {
-//    definition {
-////        cps {
-////            script(readFileFromWorkspace('src/jobs/src/dealworks-app.groovy'))
-////            sandbox()
-////        }
-//        cpsScm {
-//            scm {
-//                git {
-//                    remote {
-//                        branch('develop')
-//                        url('https://github.com/wildbuffalo/dealworks-app.git')
-//                        credentials('github-user')
-//                    }
-////                    extensions {
-////                        cleanAfterCheckout()
-////                        relativeTargetDirectory('repo1')
-////                    }
-//                    scriptPath('src/jobs/src/dealworks-app.groovy')
-//                }
-//            }
+pipelineJob('build-dealworks-app') {
+    definition {
+//        cps {
+//            script(readFileFromWorkspace('src/jobs/src/dealworks-app.groovy'))
+//            sandbox()
 //        }
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        branch('develop')
+                        github('wildbuffalo/job-dsl-gradle-example')
+                        credentials('github-user')
+                    }
+//                    extensions {
+//                        cleanAfterCheckout()
+//                        relativeTargetDirectory('repo1')
+//                    }
+                    scriptPath('src/jobs/src/dealworks-app.groovy')
+                }
+            }
+        }
+    }
+    triggers {
+        githubPush()
+    }
+}
+
+//listView('QA') {
+//    description('All unstable jobs for project A')
+//    filterBuildQueue()
+//    filterExecutors()
+//    jobs {
+////        name('release-projectA')
+//        regex(/QA-.*/)
 //    }
-//    triggers {
-//        githubPush()
+////    jobFilters {
+////        status {
+////            status(Status.UNSTABLE)
+////        }
+////    }
+//    columns {
+//        status()
+//        weather()
+//        name()
+//        lastSuccess()
+//        lastFailure()
+//        lastDuration()
+//        buildButton()
 //    }
 //}
 
-listView('QA') {
-    description('All unstable jobs for project A')
-    filterBuildQueue()
-    filterExecutors()
-    jobs {
-//        name('release-projectA')
-        regex(/QA-.*/)
-    }
-//    jobFilters {
-//        status {
-//            status(Status.UNSTABLE)
-//        }
+//buildMonitorView('project-A') {
+//    description('All jobs for project A')
+//    jobs {
+////        name('release-projectA')
+//        regex(/.*/)
 //    }
-    columns {
-        status()
-        weather()
-        name()
-        lastSuccess()
-        lastFailure()
-        lastDuration()
-        buildButton()
-    }
-}
-buildMonitorView('project-A') {
-    description('All jobs for project A')
-    jobs {
-//        name('release-projectA')
-        regex(/.*/)
-    }
-    statusFilter(StatusFilter.ENABLED)
-    filterBuildQueue()
-    recurse()
-}
+//    statusFilter(StatusFilter.ENABLED)
+//    filterBuildQueue()
+//    recurse()
+//}
