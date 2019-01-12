6ba133fe309a2844ec09bb27ac4cf6a640ca0f5b
wildbuffalo <feirenliu@gmail.com>
2019-01-11 21:56:21 -0500

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- src/jobs/build_jobs.groovy
+++ src/jobs/build_jobs.groovy
@@ -4,34 +4,70 @@ String basePath = 'JOBS'
 folder(basePath) {
     description 'This example shows how to create a set of jobs for each github branch, each in its own folder.'
 }
+multibranchPipelineJob("$basePath/build-dealworks-app") {
+    branchSources {
+        // Sets the GitHub API URI.
+//        apiUri(String apiUri)
+// Build origin branches.
+        buildOriginBranch(true)
+// Build origin branches also filed as PRs.
+        buildOriginBranchWithPR(true)
+// Build origin PRs (unmerged head).
+//        buildOriginPRHead(boolean buildOriginPRHead = true)
+// Build origin PRs (merged with base branch).
+        buildOriginPRMerge(true)
+// Sets checkout credentials for authentication with GitHub.
+        checkoutCredentialsId("github-user")
+// Sets a pattern for branches to exclude.
+//        excludes(String excludes)
 
-pipelineJob("$basePath/build-dealworks-app") {
-    definition {
-//        cps {
-//            script(readFileFromWorkspace('src/jobs/src/dealworks-app.groovy'))
-//            sandbox()
-//        }
-        cpsScm {
-            scm {
-                git {
-                    branch('master')
-                    remote {
-                        github('wildbuffalo/job-dsl-gradle-example')
-                        credentials('github-user')
-                    }
-//                    extensions {
-//                        cleanAfterCheckout()
-//                        relativeTargetDirectory('repo1')
-//                    }
-                    scriptPath('src/jobs/src/dealworks-app.groovy')
-                }
-            }
+// Sets a pattern for branches to include.
+        includes("develop,master,stage")
+// Sets the name of the GitHub Organization or GitHub User Account.
+        repoOwner("MerrillCorporation")
+// Sets the name of the GitHub repository.
+        repository("dealworks-app")
+// Sets scan credentials for authentication with GitHub.
+        scanCredentialsId("github-user")
+    }
+    orphanedItemStrategy {
+        // Trims dead items by the number of days or the number of items.
+        discardOldItems {
+// Sets the number of days to keep old items.
+            daysToKeep()
+// Sets the number of old items to keep.
+            numToKeep(5)
         }
     }
-    triggers {
-        githubPush()
-    }
+
 }
+//pipelineJob("$basePath/build-dealworks-app") {
+//    definition {
+////        cps {
+////            script(readFileFromWorkspace('src/jobs/src/dealworks-app.groovy'))
+////            sandbox()
+////        }
+//        cpsScm {
+//            scm {
+//                git {
+//                    branch('master')
+//                    remote {
+//                        github('wildbuffalo/job-dsl-gradle-example')
+//                        credentials('github-user')
+//                    }
+////                    extensions {
+////                        cleanAfterCheckout()
+////                        relativeTargetDirectory('repo1')
+////                    }
+//                    scriptPath('src/jobs/src/dealworks-app.groovy')
+//                }
+//            }
+//        }
+//    }
+//    triggers {
+//        githubPush()
+//    }
+//}
 
 //listView('QA') {
 //    description('All unstable jobs for project A')
