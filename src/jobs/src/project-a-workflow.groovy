fa18c670a77f30745b17186de00171f2b5c25995
wildbuffalo <feirenliu@gmail.com>
2019-02-01 11:49:56 -0500

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- src/jobs/src/project-a-workflow.groovy
+++ src/jobs/src/project-a-workflow.groovy
@@ -81,7 +81,7 @@ pipeline {
             }
             post {
                 always {
-                    cucumberSlackSend channel: 'zeng liu', json: 'cucumber.json'
+                    cucumberSlackSend channel: 'alrt-ds1-marketing', json: 'cucumber.json'
                 }
             }
         }
