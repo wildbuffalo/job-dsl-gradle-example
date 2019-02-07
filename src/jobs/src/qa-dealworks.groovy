11dc675272fab2601da0f9fe598fb9d13d72c493
wildbuffalo <feirenliu@gmail.com>
2019-02-07 17:06:33 -0500

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- src/jobs/src/qa-dealworks.groovy
+++ src/jobs/src/qa-dealworks.groovy
@@ -44,7 +44,7 @@ pipeline {
                             sh "cd /home/jenkins/app/ &&\
                                         ls"
 //                            sh "bundle exec parallel_cucumber features/ -n $params.threads -o \"-t @$params.tag env=$params.env sys=$params.system jobExecutionPlatform=jenkins -f json --out cucumber.json --retry 1\" "
-                            sh "bundle exec parallel_cucumber features/ -n $params.threads -o \"-t @$params.tag env=$params.env sys=$params.system jobExecutionPlatform=jenkins -f summary -f json --out cucumber.json --retry 1\""
+                            sh "bundle exec parallel_cucumber features/ -n $params.threads -o \"-t @$params.tag env=$params.env sys=$params.system jobExecutionPlatform=jenkins -f json --retry 1\"| tail -n +2| head -n -2| tee cucumber.json "
 
                         }
                     }
