b20687fd7822f6acc4fcf975ac07abd70dfa7777
wildbuffalo <feirenliu@gmail.com>
2019-02-09 01:02:54 -0500

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- src/jobs/src/qa-dealworks.groovy
+++ src/jobs/src/qa-dealworks.groovy
@@ -57,16 +57,16 @@ pipeline {
                 always {
                     script {
                         sh  'ls'
-                        step([$class: 'XUnitBuilder',
-                              thresholds: [
-                                      [$class: 'SkippedThreshold', failureThreshold: '0'],
-                                      // Allow for a significant number of failures
-                                      // Keeping this threshold so that overwhelming failures are guaranteed
-                                      //     to still fail the build
-                                      [$class: 'FailedThreshold', failureThreshold: '10']],
-                              tools: [[$class: 'JUnitType', pattern: 'junit/**']]])
+//                        step([$class: 'XUnitBuilder',
+//                              thresholds: [
+//                                      [$class: 'SkippedThreshold', failureThreshold: '0'],
+//                                      // Allow for a significant number of failures
+//                                      // Keeping this threshold so that overwhelming failures are guaranteed
+//                                      //     to still fail the build
+//                                      [$class: 'FailedThreshold', failureThreshold: '10']],
+//                              tools: [[$class: 'JUnitType', pattern: 'junit/**']]])
 
-//                        step([$class: 'JUnitResultArchiver', testDataPublishers: [[$class: 'SauceOnDemandReportPublisher', jobVisibility: 'public']], testResults: 'junit/*.xml'])
+                        step([$class: 'JUnitResultArchiver', testDataPublishers: [[$class: 'SauceOnDemandReportPublisher', jobVisibility: 'public']], testResults: 'junit/*.xml'])
                         saucePublisher()
                         cucumber 'cucumber.json'
                         sh 'cat cucumber.json'
