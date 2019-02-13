cf22b5766c447f141ef4d8182755d00a46f0d9ee
wildbuffalo <feirenliu@gmail.com>
2019-02-13 12:53:00 -0500

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- src/jobs/src/qa-dealworks.groovy
+++ src/jobs/src/qa-dealworks.groovy
@@ -1,4 +1,7 @@
 package src
+
+import groovy.json.*
+
 @Library('ds1-marketing-jenkins-library@master') _
 pipeline {
     agent any
@@ -8,6 +11,15 @@ pipeline {
         sauce('saucelabs')
     }
     post {
+        success {
+            slackMessage("good")
+        }
+        unstable {
+            slackMessage("danger")
+        }
+        failure {
+            slackMessage("danger")
+        }
         cleanup {
             cleanWs()
             dir("${env.WORKSPACE}@tmp") {
@@ -41,7 +53,7 @@ pipeline {
                             sh "ruby ./generateReport.rb"
                             sh 'cat testResults.json'
                             def props = readJSON file: 'testResults.json'
-                            sh "$props.report.totalScenarios"
+
                         }
                     }
                 }
@@ -88,4 +100,45 @@ RUN bundle install --path /home/jenkins/bundle
 
 COPY . .'''
 
+}
+def slackMessage(colorCode) {
+    script{
+        attachmenPayload = [[
+                                    fallback  : "${env.JOB_NAME} execution #${env.BUILD_NUMBER}",
+                                    color     : colorCode,
+                                    title     : "${env.JOB_NAME}",
+                                    title_link: "${env.RUN_DISPLAY_URL}",
+                                    text      : "",
+                                    fields    :
+                                            [
+                                                    [
+                                                            title: "Scenarios",
+                                                            value: props.report.totalScenarios,
+                                                            short: true
+                                                    ], [
+                                                            title: "Failed",
+                                                            value: props.report.totalFailed,
+                                                            short: true
+
+                                                    ], [
+                                                            title: "Success",
+                                                            value: props.report.totalSuccess,
+                                                            short: true
+                                                    ], [
+                                                            title: "Skipped",
+                                                            value: props.report.totalSkipped,
+                                                            short: true
+                                                    ], [
+                                                            title: "Undefined",
+                                                            value: props.report.totalUndefined,
+                                                            short: true
+                                                    ],[
+                                                            title: "Duration",
+                                                            value: props.report.totalDuration,
+                                                            short: true
+                                                    ]
+                                            ]
+                            ]]
+        slackSend(channel: '#jenkins_test', color: colorCode, attachments: new JsonBuilder(attachmenPayload).toPrettyString())
+    }
 }
