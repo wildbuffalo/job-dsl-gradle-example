8fba2d80ba9deee4749eec185ca2192a5d261fdf
wildbuffalo <feirenliu@gmail.com>
2019-02-15 15:07:25 -0500

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- src/jobs/src/promotion.groovy
+++ src/jobs/src/promotion.groovy
@@ -50,7 +50,7 @@ def scmPromote(){
                   userRemoteConfigs: [[credentialsId: '6331db84-0ca0-4396-a946-afa1e804158f', url: "https://github.com/wildbuffalo/${config.repo}.git"]]
 
         ])
-        withCredentials([usernamePassword(credentialsId: '6331db84-0ca0-4396-a946-afa1e804158f', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+        withCredentials([usernamePassword(credentialsId: 'github-user', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
 //            sh("git tag -a some_tag -m 'Jenkins'")
 //            sh('git push https://${GIT_USERNAME}:${GIT_PASSWORD}@<REPO> --tags')
             dir("${config.repo}"){
