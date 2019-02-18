234e960b5f19a1b4623719fda123dfa97ef7bc8c
wildbuffalo <feirenliu@gmail.com>
2019-02-18 12:38:20 -0500

    Merge branch 'develop' of https://github.com/MerrillCorporation/dealworks-app into develop
    # Conflicts:
    #	package-lock.json

--- src/jobs/src/promotion.groovy
+++ src/jobs/src/promotion.groovy
@@ -31,7 +31,7 @@ pipeline {
         }
         stage('Build') {
             when {
-                not{
+                anyOf {
                     changeset "*"
                 }
             }
